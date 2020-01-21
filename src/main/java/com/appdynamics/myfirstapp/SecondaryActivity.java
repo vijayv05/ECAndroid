d392a03a7f02e4e84e4534e2b41336c1bbb90f97
Vijay Venkatarajulu <vijay.venkatarajulu@appdynamics.com>
2020-01-21 21:00:15 +0530

    FirstComit

---
+++ src/main/java/com/appdynamics/myfirstapp/SecondaryActivity.java
@@ -0,0 +1,19 @@
+package com.appdynamics.myfirstapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_secondary);
+    }
+
+}
